406518235361aaeb87d60c7bb1810fad018cc0fa
m6ttw <matthewrhyswilliams@live.com>
2021-01-05 22:37:58 +0000

    forgot to do any commits so this is it

--- scope_variables.js
+++ scope_variables.js
@@ -12,5 +12,180 @@ const scenario = {
   const verdict = declareMurderer();
   console.log(verdict);
 
-// I think this looks fine and will output "The murderer is Miss Scarlet"
+// This looks fine and will output "The murderer is Miss Scarlet"
 
+// 2
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// There is an attempt to reassign a constant variable to a different value which JavaScript doesn't allow 
+
+// 3
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// I got this wrong, I predicted that both verdicts would return Mrs Peacock because let was reassigned but they didn't. I think this is because 'secondVerdict' can't see the 'let' declaration in 'declareMurderer' and so only has the initial information 'murderer = Professor Plum'
+
+// 4
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+// Similar to #3, I don't think the new let declaration inside 'declareAllSuspects' can be seen from outside, so although console.log(suspects) will return the updated suspectThree, the line underneath won't return the updated suspectThree because it doesn't know it has been reassigned
+
+// 5
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+
+// changeWeapon is called and changes the weapon to a Revolver before the declareWeapon function is called so the weapon is updated to a revolver in the verdict
+
+// 6
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Firstly the murderer is assigned with a let as Colonel Mustard, changeMurderer reassigns it to Mr Green, the plot twist function within it changes the murderer to Mrs White and this is who will be returned as the murderer in the verdict
+
+// 7
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// I expected this to return Miss Scarlet and I don't know why it doesn't although I suspect it's something to do with the 'let' in plotTwist. In what way I'm not sure
+
+// 8
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+
+// In changeScenario there are checks to see if conditions have been changed to match what is expected, one for the room which in turn reassigns the murderer, and one for the murderer which in turn reassigns the weapon. These will pass and the weapon will be reassigned from Lead Pipe to Candlestick
+
+// 9
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The verdict will return Professor Plum because of the use of 'let' to try to reassign the murderer as Mrs Peacock, it can't be seen from outside
